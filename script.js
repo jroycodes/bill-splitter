f2b3b82bfd709469455b15ea9b8f94d8180a0880
Jonathan Roy <jonaisroy@gmail.com>
2024-05-28 15:03:35 +0300

    Added basic calculator functions

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// STEPS
+// 1. Add basic calculator functions
+
+// Addition
+const addition = function (num1, num2) {
+  return num1 + num2;
+};
+addition();
+
+// Multiply
+const multiply = function (num1, num2) {
+  return num1 * num2;
+};
+multiply();
+
+// Divide
+const divide = function (num1, num2) {
+  return num1 / num2;
+};
+divide();
+
+// 2. Calculate Tip Amount per person
+// Divide Bill by 100
+// Multiply the result by selected Tip percentage
+// Divide result by number of people
+
+// 3. Add room for custom tip percentage
+
+// 4. Calculate Total per person
+// Divide bill by number of people
+// Add tip amount to result
+
+// 5. Add reset button
