c7b515fb3ea654c59725c939a6c1c872760bb6a3
Jonathan Roy <jonaisroy@gmail.com>
2024-05-29 12:03:36 +0300

    Added function to add tips per person to total per person and display results on screen

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ const billInput = document.getElementById("bill-input");
 
 billInput.addEventListener("change", function (event) {
   userInput = parseFloat(event.target.value);
+  console.log(userInput);
 });
 
 // Listen for and store tip percentage
@@ -47,11 +48,31 @@ function add(bill) {
 let people = 0;
 
 const numberOfPeople = document.getElementById("total-people");
+// target result div
+const result1Div = document.getElementById("amount1");
+const result2Div = document.getElementById("amount2");
 
 numberOfPeople.addEventListener("change", function (event) {
   people = parseFloat(event.target.value);
-  console.log(add(userInput / people));
+  console.log(people);
+  // store total per person in tips 
+  const tips = userInput / people;
+  console.log(tips);
+  // run add function on tips 
+  const tipPerPerson = add(tips).toFixed(2);
+  // add tip per person to total per person 
+    const totalPerPerson = parseFloat(tips) + parseFloat(tipPerPerson);
+    console.log(totalPerPerson);
+  //  display tip
+  result1Div.textContent = tipPerPerson;
+
+  // display total per person 
+  result2Div.textContent = totalPerPerson
+  
+  console.log("Total per person : ", totalPerPerson);
+  console.log("Tip per Person :", tipPerPerson);
 });
+
 
 // Default tip functionality
 let defaultTip = 0;
@@ -61,6 +82,6 @@ const selectedTip = document.querySelectorAll(".tips");
 selectedTip.forEach(function (tip) {
   tip.addEventListener("click", function (event) {
     defaultTip = parseFloat(event.target.textContent);
-    console.log(defaultTip);
+    console.log("Default tip :", defaultTip);
   });
 });
